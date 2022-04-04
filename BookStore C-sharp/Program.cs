43256359f487fee1a2a91f9a0aefa559838d49c1
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-04 15:19:00 +0200

    esercizio ok

--- BookStore C-sharp/Program.cs
+++ BookStore C-sharp/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string Titolo = "CLEAN CODE";
+string Autore = "Robert C.Martin";
+long ISBN = 9780132350884;
+int NumeroPagine = 431;
+int PesoLibro = 660;
+double larghezza = 17.91;
+double Profondità = 2.54;
+double Altezza = 23.5;
+double Recensione = 4.7;
+int NumeroRecensioni = 3309;
+bool VersioneKindle = true;
+bool CopertinaFlessibile = true;
+
+Console.WriteLine("--- IL LIBRO DI OGGI: " + Titolo + " di " + Autore);
+Console.WriteLine("Informazioni Generiche:");
+Console.WriteLine("ISBN: " + ISBN);
+Console.WriteLine("Numero delle pagine: " + NumeroPagine);
+Console.WriteLine("Peso del Libro: " + PesoLibro + " g");
+Console.WriteLine("Dimensioni del libro: " + larghezza + " cm x " + Altezza + " cm x " + Profondità + " cm");
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni: " + NumeroRecensioni + " recensioni");
+Console.WriteLine("Valutazione media: " + Recensione + " stelline");
+Console.WriteLine("Kindle disponibile: " + VersioneKindle);
+Console.WriteLine("Copertina flessibile disponibile: " + CopertinaFlessibile);
